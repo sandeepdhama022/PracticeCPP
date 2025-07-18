29d760b799da53383d96566ac62d1cb235d0de26
Sandeep Dhama <sandeepdhama0894@gmail.com>
2025-07-18 23:03:07 +0530

    update recursion code

--- 10_recursion/functionFactorial.cpp
+++ 10_recursion/functionFactorial.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int factorial(int number){
-    
     if ( number > 1 ){
-
         return number * factorial( number - 1 );
-
     } else {
-        
         return 1; // last case for 1!
-
     }
 }
 
 int main() {
-
     int number;
     cout << "Enter a positive number : ";
     cin >> number;
--- 10_recursion/functionSumRange.cpp
+++ 10_recursion/functionSumRange.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int sum(int number){
-    
     if ( number >= 1 ){
-
         return number + sum( number - 1 );
-
     } else {
-        
         return 0;
-
     }
 }
 
 int main() {
-
     int number;
     cout << "Enter a positive number : ";
     cin >> number;
     cout << "Sum of all number below " << number << " is " << sum(number) << endl;
     return 0;
-    
 }
